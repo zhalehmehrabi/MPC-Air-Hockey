a1b2fce4f62ab0a952dd65fb8d62e86679869066
Amirhossein zhalehmehrabi <a.h.zhalehmehraby@gmail.com>
2023-05-09 09:08:05 +0200

    matlab coonection ok
    connected with python and ok

--- Sim_2_MPC_Robot_PS_mul_shooting.m
+++ Sim_2_MPC_Robot_PS_mul_shooting.m
@@ -12,8 +12,12 @@ addpath('F:\Programs\CasadiMatlab\casadi-3.6.2-windows64-matlab2018b')
 
 import casadi.*
 
+connection = tcpclient('localhost', 8080);
+fopen(connection)
+
+
 T = 0.02; %[s]
-N = 10; % prediction horizon
+N = 20; % prediction horizon
 rob_diam = 0.3;
 
 r_mallet = 0.04815;
@@ -69,9 +73,9 @@ X = SX.sym('X',n_states,(N+1));
 obj = 0; % Objective function
 g = [];  % constraints vector
 
-Q = zeros(3,3); Q(1,1) = 1;Q(2,2) = 5;Q(3,3) = 0.1; % weighing matrices (states)
-R = zeros(6,6); R(1,1) = 0.1; R(2,2) = 0.1;R(3,3) = 0.1;
-                R(4,4) = 0.1; R(5,5) = 0.1;R(6,6) = 0.1;% weighing matrices (controls)
+Q = zeros(3,3); Q(1,1) = 100;Q(2,2) = 100;Q(3,3) = 100; % weighing matrices (states)
+R = zeros(6,6); R(1,1) = 1; R(2,2) = 1;R(3,3) = 1;
+                R(4,4) = 1; R(5,5) = 1;R(6,6) = 1;% weighing matrices (controls)
 
 
 st  = X(:,1); % initial state
@@ -107,8 +111,12 @@ args.lbx(1:n_states:n_states*(N+1),1) = x_lower; %state x lower bound
 args.ubx(1:n_states:n_states*(N+1),1) = x_upper; %state x upper bound
 args.lbx(2:n_states:n_states*(N+1),1) = y_lower; %state y lower bound
 args.ubx(2:n_states:n_states*(N+1),1) = y_upper; %state y upper bound
-args.lbx(3:n_states:n_states*(N+1),1) = 0.1; %state z lower bound
-args.ubx(3:n_states:n_states*(N+1),1) = 0.1; %state z upper bound
+%args.lbx(3:n_states:n_states*(N+1),1) = 0.1; %state z lower bound
+%args.ubx(3:n_states:n_states*(N+1),1) = 0.1; %state z upper bound
+
+args.lbx(3:n_states:n_states*(N+1),1) = 0; %state z lower bound
+args.ubx(3:n_states:n_states*(N+1),1) = 0; %state z upper bound
+
 
 args.lbx(3*(N+1)+1:n_controls:3*(N+1)+n_controls*N,1) = pos_min(1); %po1 lower bound
 args.ubx(3*(N+1)+1:n_controls:3*(N+1)+n_controls*N,1) = pos_max(1); %pos1 upper bound
@@ -130,8 +138,8 @@ args.ubx(3*(N+1)+6:n_controls:3*(N+1)+n_controls*N,1) = vel_max(3); %omega upper
 % THE SIMULATION LOOP SHOULD START FROM HERE
 %-------------------------------------------
 t0 = 0;
-x0 = [0 ; 0 ; 0.1];    % initial condition.
-xs = [-0.5 ; 0 ; 0.1]; % Reference posture.
+x0 = [-0.8600679825633534; 2.054985850341584e-05; 0.0];    % initial condition.
+xs = [-0.6 ; -0.1 ; 0.0]; % Reference posture.
 
 xx(:,1) = x0; % xx contains the history of states
 t(1) = t0;
@@ -150,10 +158,12 @@ u_cl=[];
 % than 10^-6 and the number of mpc steps is less than its maximum
 % value.
 tic
-while(norm((x0-xs),2) > 1e-2 && mpciter < sim_tim / T)
+while(norm((x0-xs),2) > 1e-2 && mpciter < sim_tim / T && mpciter < 100)
     args.p   = [x0;xs]; % set the values of the parameters vector
     % initial value of the optimization variables
+    %args.x0  = [reshape(X0',n_states*(N+1),1);reshape(u0',n_controls*N,1)];
     args.x0  = [reshape(X0',n_states*(N+1),1);reshape(u0',n_controls*N,1)];
+
     sol = solver('x0', args.x0, 'lbx', args.lbx, 'ubx', args.ubx,...
         'lbg', args.lbg, 'ubg', args.ubg,'p',args.p);
     u = reshape(full(sol.x(n_states*(N+1)+1:end))',n_controls,N)'; % get controls only from the solution
@@ -161,7 +171,7 @@ while(norm((x0-xs),2) > 1e-2 && mpciter < sim_tim / T)
     u_cl= [u_cl ; u(1,:)];
     t(mpciter+1) = t0;
     % Apply the control and shift the solution
-    [t0, x0, u0] = shift(T, t0, x0, u,n_controls);
+    [t0, x0, u0] = shift(T, t0, x0, u,n_controls, connection);
     xx(:,mpciter+2) = x0;
     X0 = reshape(full(sol.x(1:3*(N+1)))',3,N+1)'; % get solution TRAJECTORY
     % Shift trajectory to initialize the next step
@@ -173,5 +183,5 @@ toc
 
 ss_error = norm((x0-xs),2)
 Draw_MPC_point_stabilization_v1 (t,xx,xx1,u_cl,xs,N,rob_diam)
-
+fclose(t);
 
